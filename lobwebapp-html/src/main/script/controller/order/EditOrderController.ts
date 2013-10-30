b94e65ad77dc4b76908003f8b3edb0217a49cb25
Lucas Espindola <lucas.espindola@outlook.com>
2013-10-30 13:44:00 -0200

    form-validation css for ng-dirty, invalid etc...

--- lobwebapp-html/src/main/script/controller/order/EditOrderController.ts
+++ lobwebapp-html/src/main/script/controller/order/EditOrderController.ts
@@ -5,15 +5,15 @@ import Enumerable = require("linqjs");
 export module controller.order{
     interface Status{ content: string; type: string; }
     export interface EditOrderViewModel extends d.controller.base.ViewModel{
-        clientId: number;
-        productId: number;
-        productQuantity: number;
-        order: domain.Order;
+    	clientId: number;
+    	productId: number;
+    	productQuantity: number;
+    	order: domain.Order;
         isNewOrder: boolean;
         total: number;
-        addProduct: () => void;
-        removeProduct: (index: number) => void;
-        findProduct: () => void;
+        addProduct: (product: domain.Product) => void;
+        removeProduct: (id: number) => void;
+        lookupProduct: () => void;
     }
 
     export class EditOrderController implements d.controller.base.Controller{
@@ -24,26 +24,36 @@ export module controller.order{
             this.populateScope();
         }
 
-        addProduct() {
+        addProduct(product: domain.Product) {
             var exists = false;
             this.$scope.order.products.some((x: domain.Product) => {
-                if (x.id == this.$scope.productId) {
-                    x.quantity += this.$scope.productQuantity;
+                if (x.id == product.id) {
+                    x.quantity += product.quantity;
                     exists = true;
                     return true;
                 }
-                });
-            if (exists) return;
+            });
+            if (!exists){
+	            this.ProductService.findById(product.id, 
+	                (successData: domain.Product) => {
+	                    successData.quantity = product.quantity;
+	                    this.$scope.order.products.push(successData);
+	                }, (errorData) => { });
+        	}
+    	}
 
-            this.ProductService.findById(this.$scope.productId, 
-                (successData: domain.Product) => {
-                    successData.quantity = this.$scope.productQuantity;
-                    this.$scope.order.products.push(successData);
-                }, (errorData) => { });
+        removeProduct(id: number){
+        	this.$scope.order.products.some((x: domain.Product, index: number)=> {
+        			if(x.id == id){
+        				this.$scope.order.products.splice(index, 1);
+        				return true;
+        			}
+        		});
         }
 
-        removeProduct(index: number){
-            this.$scope.order.products.splice(index, 1);
+        lookupProduct() {
+            var preparedUrl = "/order/" + (this.isNewOrder() ? "new" : String(this.$scope.order.id));
+            this.$scope.navigator.navigateTo("/product/list?redirect=" + preparedUrl);
         }
 
         total(){
@@ -53,16 +63,11 @@ export module controller.order{
                     sum += x.quantity * x.price;
                 });
                 this.$scope.total = sum;
-            },500);
+            },300);
         }
 
         isNewOrder() {
             return (this.$scope.order.id == 0);
-        }
-
-        findProduct() {
-            var preparedUrl = "/order/" + (this.isNewOrder() ? "new" : String(this.$scope.order.id));
-            this.$scope.navigator.navigateTo("/product/list?redirect=" + preparedUrl);
         }
 
         processArgs(){
@@ -72,16 +77,17 @@ export module controller.order{
 
         populateScope(){
             this.$scope.$watch("order", (newValue: domain.Order, oldValue: domain.Order) => {
+                console.log("Object order changed");
                 this.$scope.isNewOrder = this.isNewOrder();
             });
             this.$scope.$watch("order.products", (newValue: domain.Product[], oldValue: domain.Product[]) => {
-                console.log("order.products mudou");
+                console.log("Object order.products changed");
                 this.total();
             }, true);
             this.$scope.order = {id: 0, client: "", products: [], status: []};      
-            this.$scope.addProduct = () => this.addProduct();
+            this.$scope.addProduct = (product: domain.Product) => this.addProduct(product);
             this.$scope.removeProduct = (index: number) => this.removeProduct(index);
-            this.$scope.findProduct = () => this.findProduct();
+            this.$scope.lookupProduct = () => this.lookupProduct();
         }
     }
 }
