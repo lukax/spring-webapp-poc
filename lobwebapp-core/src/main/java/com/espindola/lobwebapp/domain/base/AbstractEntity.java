bbfa71826a76bfe09422b81bd15b558a04085e30
Lucas Espindola <lucas.espindola@outlook.com>
2014-03-01 16:33:02 -0300

    switched db to postgres

--- lobwebapp-core/src/main/java/com/espindola/lobwebapp/domain/base/AbstractEntity.java
+++ lobwebapp-core/src/main/java/com/espindola/lobwebapp/domain/base/AbstractEntity.java
@@ -1,6 +1,7 @@
 package com.espindola.lobwebapp.domain.base;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -8,7 +9,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class AbstractEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
