9a3838eef26dddd49de71f10c11ab340e8aa4560
Lucas Espindola <lucas.espindola@outlook.com>
2014-03-03 16:10:29 -0300

    remove alerts after a location change

--- lobwebapp-html/src/script/controller/AlertController.ts
+++ lobwebapp-html/src/script/controller/AlertController.ts
@@ -5,10 +5,19 @@
 
 export module controller{
     export class AlertController {
-        static $inject = ['$scope', 'AlertService'];
-        constructor($scope, AlertService: d.service.contract.AlertService) {
+        static $inject = ["$scope", "AlertService"];
+        constructor(public $scope, public AlertService: d.service.contract.AlertService) {
             
-            $scope.alerts = AlertService.list();
+            //Simplify this controller
+            $scope.vm = this;
+
+            $scope.$on("ALERTS_CHANGED", (event, data: domain.util.Alert[])=> {
+            	$scope.alerts = data;
+            	});
+        }
+
+        remove(alert: domain.util.Alert){
+        	this.AlertService.remove(alert);
         }
     }
 }
--- lobwebapp-html/src/script/service/mock/AlertServiceMock.ts
+++ lobwebapp-html/src/script/service/mock/AlertServiceMock.ts
@@ -4,63 +4,83 @@ import enums = require("./../../util/EnumUtil");
 
 export module service.mock {
     export class AlertServiceMock implements d.service.contract.AlertService {
-        private alerts: domain.util.Alert[];
-
-        static $inject = ['$timeout'];
-        constructor(public $timeout: ng.ITimeoutService) {
-            this.alerts = [];
-            this.continuouslyRemoveOld();
+        private alerts: domain.util.Alert[] = [];
+        private lifeSecs: number = 10;
+        
+        static $inject = ["$timeout", "$rootScope"];
+        constructor(public $timeout: ng.ITimeoutService, public $rootScope: ng.IRootScopeService) {
+            this.removeOldAlertsContinuously();
+            this.removeAllOnLocationChange();
         }
 
         add(alert: domain.util.Alert) {
             if (!alert.date) alert.date = new Date();
             if (!alert.type) alert.type = enums.AlertType.OK;
-            this.alerts.push(alert);
+            var alerts = this.list();
+            alerts.push(alert);
+            this.setAlerts(alerts);
             return alert;
         }
 
         addMessageResponse(messageResponse: domain.util.MessageResponse, title: string) {
             var alert: domain.util.Alert = { content: messageResponse.message, title: title, type: enums.AlertType.DANGER, date: new Date() }
             if((<domain.util.ValidationMessageResponse>messageResponse).validations)
-                (<domain.util.ValidationMessageResponse>messageResponse).validations.forEach((x) => {
-                    alert.content += ", " + x.message;
-                    });
-            this.alerts.push(alert);
+            (<domain.util.ValidationMessageResponse>messageResponse).validations.forEach((x) => {
+                alert.content += ", " + x.message.toLowerCase();
+                });
+            var alerts = this.list();
+            alerts.push(alert);
+            this.setAlerts(alerts);
             return alert;
         }
 
         remove(alert: domain.util.Alert) {
-            this.alerts.some((currAlert, index) => {
-                if (alert === currAlert) {
-                    this.alerts.splice(index, 1);
+            var alerts = this.list();
+            alerts.some((x, index) => {
+                if(alert.content == x.content && alert.title == x.title) {
+                    alerts.splice(index, 1);
                     return true;
                 }
                 return false;
                 });
-        }
-
-        removeFirst(){
-            this.alerts.splice(0, 1);
+            this.setAlerts(alerts);
         }
 
         list() {
-            return this.alerts;
+            return <domain.util.Alert[]> angular.copy(this.alerts);
         }
 
-        private continuouslyRemoveOld() {
+        private setAlerts(alerts: domain.util.Alert[]){
+            if(!_.isEqual(this.alerts, alerts)){
+                this.alerts = alerts;
+                this.$rootScope.$broadcast("ALERTS_CHANGED", this.list());
+            }
+        }
+
+        private removeOldAlertsContinuously() {
             this.$timeout(() => {
-                if (this.alerts.length >= 3) {
-                    this.removeFirst();
+                var alerts = this.list();
+                if (alerts.length >= 3) {
+                    alerts = _.rest(this.list());
                 }
-                this.alerts.forEach((currAlert, index) => {
-                    var diffInSecs = Math.abs((new Date().getTime() - currAlert.date.getTime()) / 1000);
-                    if (diffInSecs > 10) {
-                        if(currAlert.type != enums.AlertType.DANGER)
-                            this.alerts.splice(index, 1);
+                alerts.forEach((x, index) => {
+                    var differenceInSecs = Math.abs((new Date().getTime() - x.date.getTime()) / 1000);
+                    if (differenceInSecs > this.lifeSecs) {
+                        if(x.type != enums.AlertType.DANGER)
+                            alerts.splice(index, 1);
                     }
                     });
-                this.continuouslyRemoveOld();
-                }, 500);
+                this.setAlerts(alerts);
+
+                this.removeOldAlertsContinuously();
+                }, 1000);
+        }
+
+        private removeAllOnLocationChange(){
+            //Remove all alerts after a location change
+            this.$rootScope.$on("$locationChangeSuccess", () => {
+                this.setAlerts([]);
+                });    
         }
 
     }
