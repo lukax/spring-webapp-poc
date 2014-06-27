c6569027aa3ace9e6d0382956dbb02fd696c34eb
Lucas Espindola <lucas.espindola@outlook.com>
2014-06-26 23:21:05 -0300

    import appconfig and webmvc to test context class

--- lobwebapp-core/src/test/java/com/espindola/lobwebapp/config/TestContextConfig.java
+++ lobwebapp-core/src/test/java/com/espindola/lobwebapp/config/TestContextConfig.java
@@ -2,17 +2,12 @@ package com.espindola.lobwebapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.espindola.lobwebapp.config.context.InfrastructureConfig;
-import com.espindola.lobwebapp.config.context.LocalizationConfig;
-import com.espindola.lobwebapp.config.context.PersistenceDevConfig;
+import com.espindola.lobwebapp.config.context.AppConfig;
 
 @Configuration
-@EnableWebMvc
-@Import({InfrastructureConfig.class,
-		 LocalizationConfig.class,
-		 PersistenceDevConfig.class})
+@Import({AppConfig.class,
+		 RestV1WebMvc.class})
 public class TestContextConfig {
 
 }
