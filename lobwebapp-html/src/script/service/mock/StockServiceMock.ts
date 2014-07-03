0cacf46a13ed3438d8c822bf56aa7883ac03f745
Lucas Espindola <lucas.espindola@outlook.com>
2014-07-02 22:02:34 -0300

    feat(html) new stock service mock

---
+++ lobwebapp-html/src/script/service/mock/StockServiceMock.ts
@@ -0,0 +1,40 @@
+///<reference path="../../reference.d.ts"/>
+
+import a = require("./base/EntityServiceMock");
+
+export module service.mock {
+    export class StockServiceMock extends a.service.mock.base.EntityServiceMock<domain.Stock> implements d.service.contract.StockService {
+
+        static $inject = ["$timeout"];
+        constructor(public $timeout: ng.ITimeoutService) {
+            super($timeout);
+
+            this.addToRepository({
+                id: 1,
+                product: { id: 1, name: "Notebook", description: "Dell Inspiron 15R Special Edition Intel Core i5-3230M 2.6 GHz 6144 MB 750 GB", quantity: 9, costPrice: 2102.30, price: 2699.00, category: "Informática/Computadores", ncm: "8471.30.19" },
+                quantity: 59,
+                minQuantity: 3,
+                maxQuantity: 100,
+                unit: "Item"
+            });
+        }
+
+        listUnit(
+            successCallback: (data: string[], status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any,
+            errorCallback: (data: domain.util.MessageResponse, status: number, headers: (headerName: string) => string, config: ng.IRequestConfig) => any) {
+            var unitList: string[] = [];
+            this.getRepository().forEach((x) => {
+                if (unitList.indexOf(x.unit) == -1)
+                    unitList.push(x.unit);
+            });
+
+            this.$timeout(() => {
+                successCallback(unitList, 200, null, null);
+            }, 1000);
+        }
+    }
+}
+
+export var register = (moduleName: string) => {
+    angular.module(moduleName).lazy.service("StockService", service.mock.StockServiceMock);
+};
