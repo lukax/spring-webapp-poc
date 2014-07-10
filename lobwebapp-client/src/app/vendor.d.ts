037c519158f97bad5a699c61082a5fbd4bec77a5
Lucas Espindola <lucas.espindola@outlook.com>
2014-07-10 00:33:11 -0300

    new folder structure

---
+++ lobwebapp-client/src/app/vendor.d.ts
@@ -0,0 +1,8 @@
+///<reference path="../../vendor/angularjs/angular.d.ts"/>
+///<reference path="../../vendor/angularjs/angular-route.d.ts"/>
+///<reference path="../../vendor/jquery/jquery.d.ts"/>
+///<reference path="../../vendor/requirejs/require.d.ts"/>
+///<reference path="../../vendor/underscore/underscore.d.ts"/>
+///<reference path="../../vendor/d3/d3.d.ts"/>
+///<reference path="../../vendor/crossfilter/crossfilter.d.ts"/>
+///<reference path="../../vendor/urijs/URI.d.ts"/>
--- lobwebapp-client/src/scripts/vendor.d.ts
+++
@@ -1,8 +0,0 @@
-///<reference path="../../ts-definitions/angularjs/angular.d.ts"/>
-///<reference path="../../ts-definitions/angularjs/angular-route.d.ts"/>
-///<reference path="../../ts-definitions/jquery/jquery.d.ts"/>
-///<reference path="../../ts-definitions/requirejs/require.d.ts"/>
-///<reference path="../../ts-definitions/underscore/underscore.d.ts"/>
-///<reference path="../../ts-definitions/d3/d3.d.ts"/>
-///<reference path="../../ts-definitions/crossfilter/crossfilter.d.ts"/>
-///<reference path="../../ts-definitions/urijs/URI.d.ts"/>
