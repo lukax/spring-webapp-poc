c5f3788bc311ed6e8bf7ed364843f3f986309a47
Lucas Espindola <lucas.espindola@outlook.com>
2014-07-10 23:54:22 -0300

    chore(client) add karma config

--- lobwebapp-client/karma/karma-unit.tpl.js
+++ lobwebapp-client/karma/karma-unit.tpl.js
@@ -1,71 +1,57 @@
-// Karma configuration
-// Generated on Sat Nov 02 2013 11:43:56 GMT-0200 (E. South America Daylight Time)
+module.exports = function ( karma ) {
+  karma.set({
+    /**
+     * From where to look for files, starting with the location of this file.
+     */
+    basePath: '../',
 
-module.exports = function(config) {
-    config.set({
+    /**
+     * This is the list of file patterns to load into the browser during testing.
+     */
+    files: [
+      <% scripts.forEach( function ( file ) { %>'<%= file %>',
+        <% }); %>
+      'src/**/*.js',
+    ],
+    exclude: [
+      'src/assets/**/*.js'
+    ],
+    frameworks: [ 'jasmine' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-phantomjs-launcher' ],
 
-        // base path, that will be used to resolve files and exclude
-        basePath: '../',
+    /**
+     * How to report, by default.
+     */
+    reporters: 'dots',
 
+    /**
+     * On which port should the browser connect, on which port is the test runner
+     * operating, and what is the URL path for the browser to use.
+     */
+    port: 9018,
+    runnerPort: 9100,
+    urlRoot: '/',
 
-        // frameworks to use
-        frameworks: ['jasmine', 'requirejs'],
+      /**
+      * Disable file watching by default.
+      */
+      autoWatch: false,
 
-
-        // list of files / patterns to load in the browser
-        files: [
-            'test/test-main.js',
-            {pattern: 'src/lib/**/*.js', included: false},
-            {pattern: 'src/script/**/*.js', included: false},
-            {pattern: 'test/unit/**/*Spec.js', included: false}
-        ],
-
-
-        // list of files to exclude
-        exclude: [
-            'src/lib/**/*Spec.js'
-        ],
-
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['progress'],
-
-
-        // web server port
-        port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
-
-
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera (has to be installed with `npm install karma-opera-launcher`)
-        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-        // - PhantomJS
-        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['PhantomJS'],
-
-
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
-
-
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false
-    });
+      /**
+      * The list of browsers to launch to test on. This includes only "Firefox" by
+      * default, but other browser names include:
+      * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
+      *
+      * Note that you can also use the executable name of the browser, like "chromium"
+      * or "firefox", but that these vary based on your operating system.
+      *
+      * You may also leave this blank and manually navigate your browser to
+      * http://localhost:9018/ when you're running tests. The window/tab can be left
+     * open and the tests will automatically occur there during the build. This has
+     * the aesthetic advantage of not launching a browser every time you save.
+     */
+    browsers: [
+      'PhantomJS'
+    ]
+  });
 };
