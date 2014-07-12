d047127caa439b3dc85a3f48530f38e736491fd2
Lucas Espindola <lucas.espindola@outlook.com>
2014-07-12 00:30:29 -0300

    chore(client) reorganize ts definitions files

--- lobwebapp-client/src/app/vendor.d.ts
+++ lobwebapp-client/src/app/vendor.d.ts
@@ -1,9 +1,9 @@
-///<reference path="../../vendor/definitions/jquery/jquery.d.ts"/>
-///<reference path="../../vendor/definitions/angular/angular.d.ts"/>
-///<reference path="../../vendor/definitions/angular/angular-route.d.ts"/>
-///<reference path="../../vendor/definitions/d3/d3.d.ts"/>
-///<reference path="../../vendor/definitions/crossfilter/crossfilter.d.ts"/>
-///<reference path="../../vendor/definitions/urijs/URI.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/jquery/jquery.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/angularjs/angular.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/angularjs/angular-route.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/d3/d3.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/crossfilter/crossfilter.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/urijs/URI.d.ts"/>
 
-///<reference path="../../vendor/definitions/angular/angular-mocks.d.ts"/>
-///<reference path="../../vendor/definitions/jasmine/jasmine.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/angularjs/angular-mocks.d.ts"/>
+///<reference path="../../vendor/DefinitelyTyped/jasmine/jasmine.d.ts"/>
